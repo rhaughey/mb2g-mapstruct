a114734a30692449a9ff291fba12b617c0cbcf12
rhaughey <rfhaughey@gmail.com>
2019-10-05 14:47:15 -0400

    Adding UserMapper interface

---
+++ src/main/java/guru/springframework/mappers/UserMapper.java
@@ -0,0 +1,18 @@
+package guru.springframework.mappers;
+
+import guru.springframework.domain.User;
+import guru.springframework.model.UserCommand;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Created by rfh on 10/5/2019
+ */
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
+}
